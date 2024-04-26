de5cd898f68c81600fbc9fe45004ca9d1c968895
ShivPandey5677 <pandeyshiv525@gmail.com>
2024-04-26 13:05:08 +0530

    Decompress added

--- backend/EncrDecr/huffman.js
+++ backend/EncrDecr/huffman.js
@@ -107,58 +107,91 @@ class Node {
 //         outputStream.end();
 //     });
 // }
-
 // function compress(inputFile, outputFile, tree, codes) {
-//     const inputStream = fs.createReadStream(inputFile, { encoding: 'utf8' });
-//     const outputStream = fs.createWriteStream(outputFile, { encoding: 'binary' });
-   
-//     // Write the Huffman codes to the output file
-//     const codesString = JSON.stringify(codes);
-//     if (!codesString) {
-//         console.error("Error: Unable to stringify Huffman codes.");
-//         return;
-//     }
-//     const codesLength = Buffer.alloc(4);
-//     codesLength.writeUInt32BE(codesString.length);
-//     outputStream.write(codesLength);
-//     outputStream.write(codesString);
+//   const inputStream = fs.createReadStream(inputFile, { encoding: 'utf8' });
+//   const outputStream = fs.createWriteStream(outputFile, { encoding: 'utf8' });
 
-//     let currentByte = '';
+//   // Write the Huffman codes to the output file
+//   const codesString = JSON.stringify(codes);
+//   if (!codesString) {
+//       console.error("Error: Unable to stringify Huffman codes.");
+//       return;
+//   }
+//   const codesLength = Buffer.alloc(4);
+//   codesLength.writeUInt32BE(codesString.length);
+//   console.log("Writing Huffman codes length:", codesString.length);
+//   outputStream.write(codesLength);
+//   console.log("Writing Huffman codes:", codesString);
+//   const codesBuffer = Buffer.from(codesString, 'utf8');
+//   outputStream.write(codesBuffer);
 
-//     inputStream.on('data', (chunk) => {
-//         for (let i = 0; i < chunk.length; i++) {
-//             const char = chunk[i];
-//             const code = tree[char];
-//             if (code !== undefined) {
-//                 currentByte += code;
-//                 // If the currentByte is a full byte, write it to the output stream
-//                 while (currentByte.length >= 8) {
-//                     const byte = currentByte.substring(0, 8);
-//                     const byteBuffer = Buffer.from(byte, 'binary');
-//                     outputStream.write(byteBuffer);
-//                     currentByte = currentByte.substring(8);
-//                 }
-//             } else {
-//                 console.error(`Character '${char}' does not have a Huffman code.`);
-//             }
-//         }
-//     });
+//   let currentByte = '';
 
-//     inputStream.on('end', () => {
-//         // If there are remaining bits in currentByte, pad them with zeros and write to the output stream
-//         if (currentByte.length > 0) {
-//             const padding = 8 - currentByte.length;
-//             currentByte += '0'.repeat(padding);
-//             const byteBuffer = Buffer.from(currentByte, 'binary');
-//             outputStream.write(byteBuffer);
-//         }
-//         outputStream.end();
-//     });
+//   inputStream.on('data', (chunk) => {
+//       for (let i = 0; i < chunk.length; i++) {
+//           const char = chunk[i];
+//           const code = tree[char];
+//           if (code !== undefined) {
+//               currentByte += code;
+//               // If the currentByte is a full byte, write it to the output stream
+//               while (currentByte.length >= 8) {
+//                   const byte = currentByte.substring(0, 8);
+//                   const byteBuffer = Buffer.from(byte, 'binary');
+//                   outputStream.write(byteBuffer);
+//                   currentByte = currentByte.substring(8);
+//               }
+//           } else {
+//               console.error(`Character '${char}' does not have a Huffman code.`);
+//           }
+//       }
+//   });
+
+//   inputStream.on('end', () => {
+//       // If there are remaining bits in currentByte, pad them with zeros and write to the output stream
+//       if (currentByte.length > 0) {
+//           const padding = 8 - currentByte.length;
+//           currentByte += '0'.repeat(padding);
+//           const byteBuffer = Buffer.from(currentByte, 'binary');
+//           outputStream.write(byteBuffer);
+//       }
+//       // Include the compressed data as a property of the object of codes under the "data" key
+//       codes.data = currentByte;
+//       outputStream.end();
+//   });
 // }
+
+function compress(inputFile, outputFile, tree, codes) {
+  const inputStream = fs.createReadStream(inputFile, { encoding: 'utf8' });
+  const outputStream = fs.createWriteStream(outputFile, { encoding: 'utf8' });
+
+  let compressedData = ''; // Variable to store compressed data
+
+  // Compress the input text
+  inputStream.on('data', (chunk) => {
+      for (let i = 0; i < chunk.length; i++) {
+          const char = chunk[i];
+          const code = tree[char];
+          if (code !== undefined) {
+              compressedData += code;
+          } else {
+              console.error(`Character '${char}' does not have a Huffman code.`);
+          }
+      }
+  });
+
+  inputStream.on('end', () => {
+      // Write the Huffman codes and compressed data to the output file
+      const compressedObject = { ...codes, data: compressedData }; // Include the compressed data in the object
+      const compressedString = JSON.stringify(compressedObject);
+      outputStream.write(compressedString);
+      outputStream.end();
+  });
+}
+
 // function compress(inputFile, outputFile, tree, codes) {
 //     const inputStream = fs.createReadStream(inputFile, { encoding: 'utf8' });
 //     const outputStream = fs.createWriteStream(outputFile, { encoding: 'binary' });
-   
+
 //     // Write the Huffman codes to the output file
 //     const codesString = JSON.stringify(codes);
 //     if (!codesString) {
@@ -170,7 +203,8 @@ class Node {
 //     console.log("Writing Huffman codes length:", codesString.length);
 //     outputStream.write(codesLength);
 //     console.log("Writing Huffman codes:", codesString);
-//     outputStream.write(codesString);
+//     const codesBuffer = Buffer.from(codesString, 'utf8');
+//     outputStream.write(codesBuffer);
 
 //     let currentByte = '';
 
@@ -204,110 +238,107 @@ class Node {
 //         outputStream.end();
 //     });
 // }
-function compress(inputFile, outputFile, tree, codes) {
-    const inputStream = fs.createReadStream(inputFile, { encoding: 'utf8' });
-    const outputStream = fs.createWriteStream(outputFile, { encoding: 'binary' });
+// function decompress(inputFile, outputFile) {
+//     const inputStream = fs.createReadStream(inputFile, { encoding: 'binary' });
+//     const outputStream = fs.createWriteStream(outputFile, { encoding: 'utf8' });
 
-    // Write the Huffman codes to the output file
-    const codesString = JSON.stringify(codes);
-    if (!codesString) {
-        console.error("Error: Unable to stringify Huffman codes.");
-        return;
-    }
-    const codesLength = Buffer.alloc(4);
-    codesLength.writeUInt32BE(codesString.length);
-    console.log("Writing Huffman codes length:", codesString.length);
-    outputStream.write(codesLength);
-    console.log("Writing Huffman codes:", codesString);
-    const codesBuffer = Buffer.from(codesString, 'utf8');
-    outputStream.write(codesBuffer);
+//     let codesLengthBuffer = Buffer.alloc(4);
+//     let codesLength = 0;
+//     let codesString = '';
+//     let decodedString = '';
 
-    let currentByte = '';
+//     // Read the length of Huffman codes from the compressed file
+//     inputStream.on('data', (chunk) => {
+//         if (codesLengthBuffer.length < 4) {
+//             const remainingBytes = 4 - codesLengthBuffer.length;
+//             codesLengthBuffer = Buffer.concat([codesLengthBuffer, chunk.slice(0, remainingBytes)], codesLengthBuffer.length + remainingBytes);
+//             chunk = chunk.slice(remainingBytes);
+//         }
+//         if (codesLengthBuffer.length === 4 && codesLength === 0) {
+//             codesLength = codesLengthBuffer.readUInt32BE();
+//             if (chunk.length >= codesLength) {
+//                 codesString = chunk.slice(0, codesLength).toString('utf8');
+//                 chunk = chunk.slice(codesLength);
+//             }
+//         }
 
-    inputStream.on('data', (chunk) => {
-        for (let i = 0; i < chunk.length; i++) {
-            const char = chunk[i];
-            const code = tree[char];
-            if (code !== undefined) {
-                currentByte += code;
-                // If the currentByte is a full byte, write it to the output stream
-                while (currentByte.length >= 8) {
-                    const byte = currentByte.substring(0, 8);
-                    const byteBuffer = Buffer.from(byte, 'binary');
-                    outputStream.write(byteBuffer);
-                    currentByte = currentByte.substring(8);
-                }
-            } else {
-                console.error(`Character '${char}' does not have a Huffman code.`);
-            }
-        }
-    });
+//         // Continue with decompression logic once codes are read
+//         if (codesString !== '') {
+//             const codes = JSON.parse(codesString);
 
-    inputStream.on('end', () => {
-        // If there are remaining bits in currentByte, pad them with zeros and write to the output stream
-        if (currentByte.length > 0) {
-            const padding = 8 - currentByte.length;
-            currentByte += '0'.repeat(padding);
-            const byteBuffer = Buffer.from(currentByte, 'binary');
-            outputStream.write(byteBuffer);
-        }
-        outputStream.end();
-    });
-}
+//             // Traverse the Huffman tree based on the bits in the compressed data
+//             let currentCode = '';
+//             for (let i = 0; i < chunk.length; i++) {
+//                 const byte = chunk[i];
+//                 const binaryString = byte.toString(2).padStart(8, '0'); // Convert byte to binary string
+
+//                 // Add the bits of the current byte to the current code
+//                 currentCode += binaryString;
+
+//                 // Check if the current code matches any Huffman code
+//                 for (const [char, code] of Object.entries(codes)) {
+//                     if (currentCode.startsWith(code)) {
+//                         decodedString += char;
+//                         currentCode = currentCode.slice(code.length);
+//                     }
+//                 }
+//             }
+//         }
+//     });
+
+//     inputStream.on('end', () => {
+//         // Write the decoded string to the output stream
+//         outputStream.write(decodedString);
+//         outputStream.end();
+//     });
+// }
 function decompress(inputFile, outputFile) {
-    const inputStream = fs.createReadStream(inputFile, { encoding: 'binary' });
-    const outputStream = fs.createWriteStream(outputFile, { encoding: 'utf8' });
+  // const inputStream = fs.createReadStream(inputFile, { encoding: 'utf8' });
 
-    let codesLengthBuffer = Buffer.alloc(4);
-    let codesLength = 0;
-    let codesString = '';
-    let decodedString = '';
+  const outputStream = fs.createWriteStream(outputFile, { encoding: 'utf8' });
 
-    // Read the length of Huffman codes from the compressed file
-    inputStream.on('data', (chunk) => {
-        if (codesLengthBuffer.length < 4) {
-            const remainingBytes = 4 - codesLengthBuffer.length;
-            codesLengthBuffer = Buffer.concat([codesLengthBuffer, chunk.slice(0, remainingBytes)], codesLengthBuffer.length + remainingBytes);
-            chunk = chunk.slice(remainingBytes);
-        }
-        if (codesLengthBuffer.length === 4 && codesLength === 0) {
-            codesLength = codesLengthBuffer.readUInt32BE();
-            if (chunk.length >= codesLength) {
-                codesString = chunk.slice(0, codesLength).toString('utf8');
-                chunk = chunk.slice(codesLength);
-            }
-        }
+  let compressedObjectString = '';
+  compressedObjectString=fs.readFileSync(inputFile)
+ 
+  // inputStream.on(', (chunk) => {
+  //   console.log(chunk)
+  //     compressedObjectString += chunk;
+  //     console.log("hello")
+  // });
+  
+  // inputStream.on('end', () => {
+      // Parse the compressed object
+      const compressedObject = JSON.parse(compressedObjectString);
+  console.log(compressedObject)
+      // Extract compressed data from the object
+      const compressedData = compressedObject.data;
 
-        // Continue with decompression logic once codes are read
-        if (codesString !== '') {
-            const codes = JSON.parse(codesString);
+      // Extract Huffman codes
+      const codes = {};
+      Object.assign(codes, compressedObject);
+      delete codes.data;
 
-            // Traverse the Huffman tree based on the bits in the compressed data
-            let currentCode = '';
-            for (let i = 0; i < chunk.length; i++) {
-                const byte = chunk[i];
-                const binaryString = byte.toString(2).padStart(8, '0'); // Convert byte to binary string
+      // Traverse the Huffman tree based on the bits in the compressed data
+      let currentCode = '';
+      let decodedString = '';
+      for (let i = 0; i < compressedData.length; i++) {
+          currentCode += compressedData[i];
+          for (const [char, code] of Object.entries(codes)) {
+              if (currentCode === code) {
+                  decodedString += char;
+                  currentCode = '';
+                  break;
+              }
+          }
+      }
 
-                // Add the bits of the current byte to the current code
-                currentCode += binaryString;
-
-                // Check if the current code matches any Huffman code
-                for (const [char, code] of Object.entries(codes)) {
-                    if (currentCode.startsWith(code)) {
-                        decodedString += char;
-                        currentCode = currentCode.slice(code.length);
-                    }
-                }
-            }
-        }
-    });
-
-    inputStream.on('end', () => {
-        // Write the decoded string to the output stream
-        outputStream.write(decodedString);
-        outputStream.end();
-    });
+      // Write the decoded string to the output stream
+      outputStream.write(decodedString);
+      outputStream.end();
+  // });
 }
+
+
 
   export default function huff(inputfile) {
     const inputFile = `public/uploads/${inputfile}`;
@@ -336,7 +367,7 @@ function decompress(inputFile, outputFile) {
     console.log("Huffman Codes:", codes);
     
     // Compress the input text file
-    const outputFile = path.resolve('output.bin');
+    const outputFile = path.resolve('output.txt');
     // compress( inputFile,outputFile,codes,codes);
  compress(inputFile,outputFile,codes,codes)
  const decompressedFile = path.resolve('d.txt');
